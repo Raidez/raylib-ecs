cd26563bd8624d303028e39792e8207487bf6c72
Alexis LUCIEN <alexis-externe.lucien@enedis.fr>
2024-05-15 16:57:24 +0200

    first attempt to make a module

---
+++ ecs/__init__.py
@@ -0,0 +1,13 @@
+from .utils import (
+    Entity,
+    EntityProxy,
+    Component,
+    Criteria,
+    HasId,
+    HasComponent,
+    HasComponentValue,
+    HasNotComponent,
+    FilterCriteria,
+    SugarCriteria,
+    Query,
+)
